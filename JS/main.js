91b982b0f57680e674c215a8c62593ae2749ba71
Ayush Anand <direct.ayush@gmail.com>
2023-08-23 18:56:41 +0530

    Redirect working

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,20 @@
-// content.js
-
 // Function to navigate to the post-solution page
 function navigateToPostSolutionPage() {
-    const postSolutionLink = window.location.href + '/post-solution';
-    window.location.href = postSolutionLink;
-  }
-  
-  // Wait for the DOM to load
-  document.addEventListener('DOMContentLoaded', function() {
-    const postSolutionButton = document.getElementById('postSolutionButton');
-  
-    postSolutionButton.addEventListener('click', function() {
-      // Change the name of the button to "abc"
-      postSolutionButton.textContent = 'abc';
-      
-      // Navigate to the post-solution page
-      navigateToPostSolutionPage();
-    });
+  // Use the chrome.tabs API to get the active tab's ID
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const activeTabId = tabs[0].id;
+    const postSolutionLink = tabs[0].url + 'post-solution';
+
+    // Update the URL of the active tab
+    chrome.tabs.update(activeTabId, { url: postSolutionLink });
   });
-  
+}
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+  const postSolutionButton = document.getElementById('postSolutionButton');
+
+  postSolutionButton.addEventListener('click', function() {
+    navigateToPostSolutionPage();
+  });
+});
